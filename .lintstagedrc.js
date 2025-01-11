2ae5554cc8b056d5a5acf20977cb5aa958298ead
PMtHk <zooby88@gmail.com>
2025-01-11 22:08:08 +0900

    chore: husky, lint-staged 로 git-hook 추가
    - next 와 사용하기 위한 .lintstagedrc.js 생성
    - pre-commit 훅 추가

---
+++ .lintstagedrc.js
@@ -0,0 +1,12 @@
+// https://nextjs.org/docs/pages/api-reference/config/eslint#running-lint-on-staged-files
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+}
