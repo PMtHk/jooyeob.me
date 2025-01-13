e3d9d6b3035563481e6148eb95498260715c85c9
PMtHk <zooby88@gmail.com>
2025-01-13 23:59:29 +0900

    refactor: server 클라이언트의 async 외부 await 제거

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -4,9 +4,8 @@ import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import { publicEnv, getServerEnv } from '@/libs/env'
 
-const serverEnv = await getServerEnv()
-
 export async function createClient() {
+  const serverEnv = await getServerEnv()
   const cookieStore = await cookies()
 
   return createServerClient(publicEnv.SUPABASE_URL, serverEnv.SUPABASE_SERVICE_ROLE_KEY, {
