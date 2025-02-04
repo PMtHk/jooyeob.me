4fc19d9204a9e64468a0855a7d74f7bb476101c3
PMtHk <zooby88@gmail.com>
2025-02-04 19:49:40 +0900

    feat: 포스트 상세 조회 기능 작성
    - 포스트 상세 조회 서버 액션 작성
    - 포스트 페이지 정보 조회 기능 연동
    - Tag 목록 불러오기 기능 추가

--- src/app/(public)/components/Tags.tsx
+++ src/app/(public)/components/Tags.tsx
@@ -1,14 +1,20 @@
-export function Tags() {
+import { getTags } from '@/shared/libs/posts/actions'
+
+export async function Tags() {
+  const tags = await getTags()
+
   return (
     <section>
       <span className='text-default-lg text-alt-600'>태그</span>
       <div className='mt-3 flex flex-wrap'>
-        <span className='mb-2 cursor-pointer rounded-full px-2 py-1 text-body-md text-alt-700 bg-alt-100 mr-1.5 hover:bg-alt-200 dark:text-alt-300 dark:bg-alt-800 dark:hover:bg-alt-900'>
-          JavaScript
-        </span>
-        <span className='mb-2 cursor-pointer rounded-full px-2 py-1 text-body-md text-alt-700 bg-alt-100 mr-1.5 hover:bg-alt-200 dark:text-alt-300 dark:bg-alt-800 dark:hover:bg-alt-900'>
-          JavaScript
-        </span>
+        {tags.map((tag) => (
+          <span
+            key={tag.id}
+            className='mb-2 cursor-pointer rounded-full px-2 py-1 text-body-md text-alt-700 bg-alt-100 mr-1.5 hover:bg-alt-200 dark:text-alt-300 dark:bg-alt-800 dark:hover:bg-alt-900'
+          >
+            {tag.name}
+          </span>
+        ))}
       </div>
     </section>
   )
