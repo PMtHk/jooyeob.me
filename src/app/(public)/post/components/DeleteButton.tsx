03085da5002dc29c9e16d9b5b89857faf2533b9a
PMtHk <zooby88@gmail.com>
2025-02-13 16:35:16 +0900

    feat: 포스트 삭제 버튼 추가
    - 포스트 삭제 기능 연동
    경# feat: 새로운 기능 추가

---
+++ src/app/(public)/post/components/DeleteButton.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { Button } from '@/components/ui'
+import { useAuth } from '@/contexts/authContext'
+import { deletePost } from '@/lib/actions/post'
+
+export function DeleteButton({
+  id,
+}: Readonly<{
+  id: number
+}>) {
+  const { isAuthenticated } = useAuth()
+
+  if (!isAuthenticated) {
+    return null
+  }
+
+  return (
+    <Button onClick={() => deletePost(id)} variant='ghost' className='text-black dark:text-white'>
+      삭제
+    </Button>
+  )
+}
--- src/lib/util/getKST.ts
+++ src/lib/util/getKST.ts
@@ -16,8 +16,8 @@ export const getKST = (dateString: string) => {
   const year = parts.find((part) => part.type === 'year')?.value
   const month = parts.find((part) => part.type === 'month')?.value
   const day = parts.find((part) => part.type === 'day')?.value
-  const hour = parts.find((part) => part.type === 'hour')?.value
-  const minute = parts.find((part) => part.type === 'minute')?.value
+  // const hour = parts.find((part) => part.type === 'hour')?.value
+  // const minute = parts.find((part) => part.type === 'minute')?.value
 
-  return `${year}년 ${month}월 ${day}일 ${hour}:${minute}`
+  return `${year}년 ${month}월 ${day}일`
 }
